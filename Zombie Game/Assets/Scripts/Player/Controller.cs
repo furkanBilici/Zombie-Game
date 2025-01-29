33e36754d1ae8bd0098ff8b6ebb4aefdebc26943
furkanBilici <fbilici258@gmail.com>
2025-01-29 22:28:10 +0300

    proje başlangıcı

---
+++ Zombie Game/Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody body;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void wasd()
+    {
+
+    }
+    void Jump()
+    {
+
+    }
+    void Crouch()
+    {
+
+    }
+}

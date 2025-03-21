c0d65f416b47a4bf7bbd33039abf187496801ca1
Furkan-Bilici <fbilici258@gmail.com>
2025-03-21 16:26:24 +0300

    raycast ayarlamaları
    düşmanı tarayan ve ona göre hedef noktasını kırmızıya çeviren kod eklendi

--- Zombie Game/Assets/Scripts/Camera/AimDotEnabler.cs
+++ Zombie Game/Assets/Scripts/Camera/AimDotEnabler.cs
@@ -1,32 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class KillerArea : MonoBehaviour
+public class AimDotEnabler : MonoBehaviour
 {
+    public GameObject killer;
+    public GameObject aimdotRed;
     RaycastHit hit;
     public int rayCount;
     public float angleRange;
     public float distance;
+    bool set;
+
     private void Update()
     {
         float halfAngle = angleRange / 2;
+        set=false;
         for (int i = 0; i < rayCount; i++)
         {
-            float angle = Mathf.Lerp(-halfAngle, halfAngle, (float)i / (rayCount - 1));
-            Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+            float angle=Mathf.Lerp(-halfAngle,halfAngle,(float)i/(rayCount-1));
+            Vector3 direction=Quaternion.Euler(0,angle,0)*transform.forward;
 
             if (Physics.Raycast(transform.position, direction, out hit, distance))
             {
                 if (hit.collider.CompareTag("zombie"))
                 {
-                   Destroy(hit.collider.gameObject);
+                    set = true;
+                    if(killer.activeSelf)
+                    {
+                        Destroy(hit.collider.gameObject);
+                    }
                 }
             }
         }
+        aimdotRed.SetActive(set);
     }
 }
-
-
-

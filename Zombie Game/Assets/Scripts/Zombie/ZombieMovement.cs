efa03db22bd096e5caa3c249d98d97b737692f27
Furkan-Bilici <fbilici258@gmail.com>
2025-04-25 11:05:58 +0300

    zombie hareket fonksiyonu düzenlendi, daha stabil çalışacak

--- Zombie Game/Assets/Scripts/Zombie/ZombieMovement.cs
+++ Zombie Game/Assets/Scripts/Zombie/ZombieMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -9,24 +10,48 @@ public class ZombieMovement : MonoBehaviour
     public GameObject player;
     public float speed = 3f;
     public Vector3 SeekDistance;//zombinin alaný
+
+    Vector3 direction;  // Yönü normalize et
+    Vector3 newPosition;   // Sabit hýzla ilerle
+    Vector3 lookDirection;
+    Vector3 distance;
+
+    bool canChase;
+    private void Start()
+    {
+        canChase = false;
+    }
     void Update()
     {
-        Chase();
+        ChaseControll();
         CheckIfBehindPlayer();
     }
-
-    void Chase()
+    private void FixedUpdate()
     {
-        Vector3 distance = new Vector3(Mathf.Abs(body.transform.position.x - player.transform.position.x), 0, Mathf.Abs(body.transform.position.z - player.transform.position.z));
+        Chase();
+    }
+    void ChaseControll()
+    {
+        distance = new Vector3(Mathf.Abs(body.transform.position.x - player.transform.position.x), 0, Mathf.Abs(body.transform.position.z - player.transform.position.z));
 
         if (distance.z <= SeekDistance.z && distance.x <= SeekDistance.x)
         {
-            Vector3 direction = (player.transform.position - body.position).normalized;  // Yönü normalize et
-            Vector3 newPosition = body.position + direction * speed * Time.deltaTime;   // Sabit hýzla ilerle
-            body.MovePosition(newPosition);
-
-            Vector3 lookDirection = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+            canChase = true;
+            direction = (player.transform.position - body.position).normalized;  // Yönü normalize et
+            newPosition = body.position + direction * speed * Time.deltaTime;   // Sabit hýzla ilerle
+            lookDirection = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z); 
             transform.LookAt(lookDirection);
+        }
+        else
+        {
+            canChase = false;
+        }
+    }
+    void Chase()
+    {
+        if (canChase)
+        {
+            body.MovePosition(newPosition);
         }
     }
     void CheckIfBehindPlayer()
